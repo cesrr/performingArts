3bccdbbdad7a21982bfdd856e39b6baecfd9bdb4
Cesar Espinoza <114450167+cesrr@users.noreply.github.com>
2023-10-06 12:02:36 -0600

    added JS reviews, styles. Also adjusted footer

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+//navigation menu
 function toggleMenu() {
     document.getElementById("primaryNav").classList.toggle("open");
     document.getElementById("hamburger").classList.toggle("open");
@@ -6,3 +7,33 @@ function toggleMenu() {
 const x = document.getElementById('hamburger');
 
 x.onclick = toggleMenu;
+
+//reviews
+import { reviews } from './data.js';
+
+const reviewContainer = document.querySelector('#revCards');
+
+for (let i = 0; i < reviews.length; i++) {
+    let card = document.createElement('div');
+    let name = document.createElement('h3');
+    let stars = document.createElement('section');
+    let text = document.createElement('p');
+ 
+
+
+    for (let j = 0; j < reviews[i].stars; j++) {
+        const starIndicator = document.createElement('img');
+        starIndicator.src = "../images/star.png";
+        stars.appendChild(starIndicator);
+
+    }
+
+    name.textContent = reviews[i].name;
+    text.textContent = reviews[i].text;
+
+    card.appendChild(name);
+    card.appendChild(stars);
+    card.appendChild(text);
+
+    reviewContainer.appendChild(card);
+}
